daca3d0f114fb281be1db74d2fe25a415628436f
Gabriel Drai <Gabrieldrai@hotmail.fr>
2017-12-13 14:24:11 +0200

    created Registration VC

---
+++ HotelManzana/Controllers/Registration VC.swift
@@ -0,0 +1,25 @@
+//
+//  Registration VC.swift
+//  HotelManzana
+//
+//  Created by Gabriel Drai on 13/12/2017.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+
+   
+}
