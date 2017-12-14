13ea5a4541ef2461326f65f51abe803c803c1c35
Ronny Abraham <ronny.coder @gmail.com>
2017-12-14 14:37:28 +0200

    add iboutlets

--- HotelManzana/Controllers/Registration VC.swift
+++ HotelManzana/Controllers/Registration VC.swift
@@ -16,6 +16,11 @@ class RegistrationVC: UITableViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
     @IBAction func doneBarButtonItem(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
