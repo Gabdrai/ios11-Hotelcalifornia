374c0d6df219d690e3db35fd27a3cd0c76c3738d
Gabriel Drai <Gabrieldrai@hotmail.fr>
2017-12-17 12:05:16 +0200

    created the room type VC as initial VC

--- HotelManzana/Controllers/SelectRoomTypeTableViewController.swift
+++ HotelManzana/Controllers/SelectRoomTypeTableViewController.swift
@@ -17,3 +17,27 @@ static var all: [RoomType] {
             RoomType(id: 2, name: "Penthouse Suite", shortName:
                 "PHS", price: 309)]
 }
+
+class SelectRoomTypeTableViewController: UITableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+      
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return RoomType.all.count
+    }
+      
+    override func tableView(_ tableView: UITableView, cellForRowAt
+        indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "RoomTypeCell", for: indexPath)
+        let roomType = RoomType.all[indexPath.row]
+          
+        cell.textLabel?.text = roomType.name
+        cell.detailTextLabel?.text = "$ \(roomType.price)"
+          
+        return cell
+    }
+}
